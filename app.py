933e1d9d12481ab25290ec43e80f91ec62937e3f
Hamza Mehammed <hmehammed@gmail.com>
2022-04-17 21:51:20 +0200

    test ok

--- app.py
+++ app.py
@@ -6,6 +6,5 @@ app = Flask(__name__)
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
     return "".join(reversed(random_string))
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
